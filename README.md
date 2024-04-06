b1033e4dceb6ab7846c721b2d960cad5a0126c7f
tilak@ash <tsingh99991@gmail.com>
2024-04-06 09:59:59 +0530

    todo app init with nodejs operations

---
+++ README.md
@@ -0,0 +1,11 @@
+##TODO APP
+
+This projects contains a simple TODO application
+It has the following features
+
+- Anyone can create a Todo
+- Anyone can see their existings todos
+- Anyone can mark a todo as done
+
+// initialize a node project
+// put a package.json
---
+++ backEnd/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { createTodo, updateTodo } = require("../../../types");
+const { todo } = require("../../../db");
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", async (req, res) => {
+   const createPayload = req.body;
+   const parsedPayload = createTodo.safeParse(createPayload);
+   if (!parsedPayload.success) {
+      res.status(411).json({
+         msg: "you sent the wrong input",
+      });
+      return;
+   }
+   //mongo code
+   await todo.create({
+      title: createPayload.title,
+      description: createPayload.description,
+      completed: false,
+   });
+
+   res.status(200).json({
+      msg: "TODO created successfully",
+   });
+});
+app.get("/todos", async (req, res) => {
+   const todos = await todo.find();
+   res.status(200).json({
+      todos,
+   });
+});
+app.put("/completed", async (req, res) => {
+   const updatePayload = req.body;
+   const parsedPayload = updateTodo.safeParse(updatePayload);
+   if (!parsedPayload.success) {
+      res.status(411).json({
+         msg: "you sent the wrong input",
+      });
+      return;
+   }
+   //mongo code
+   await todo.update(
+      {
+         _id: req.body.id,
+      },
+      {
+         completed: true,
+      }
+   );
+   res.status(200).json({
+    msg : " You sent wrong input"
+   })
+});
+
+app.listen(PORT, () => {
+   console.log(`server is on ${PORT}`);
+});
